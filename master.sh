2a543d15796b7ef844591dcf4e7598021ec74c88
aadspinner <115710797+aadspinner@users.noreply.github.com>
2024-09-21 21:07:31 +0530

    updated with calico

--- master.sh
+++ master.sh
@@ -1,8 +1,9 @@
 
-# install only on master / controlplane 
-kubeadm init --control-plane-endpoint 13.202.18.177 --pod-network-cidr 192.168.0.0/16 
+# install only on master / controlplane
+# init config for cluster creation with public ip of ec2 instance.
+kubeadm init --control-plane-endpoint 13.202.18.177 --pod-network-cidr 192.168.0.0/16
+
+# download and install calico CNI.
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/custom-resources.yaml -O
-
-#--apiserver-advertise-address 13.202.18.177  --ignore-preflight-errors all
-#--apiserver-advertise-address 13.202.18.177  --ignore-preflight-errors all
+kubectl apply -f custom-resources.yaml
